c570846c11f15deda19a0c29f5360a5b532fe617
Luiza <luizaschettinii@gmail.com>
2025-02-18 11:20:16 -0300

    refactor:route organization

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
-const usersRoutes = require("./routes/usersRoutes");
-app.use("/api", usersRoutes);
+const usersRoutes = require("./src/routes/usersRoutes");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+app.use("/users", usersRoutes);
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
+    console.log(`🎈 Servidor rodando em http://localhost:${PORT}`);
 });
