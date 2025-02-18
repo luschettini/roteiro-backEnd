a6a5502b55b122667704506362ee920b803ae77f
Luiza <luizaschettinii@gmail.com>
2025-02-18 11:09:27 -0300

    feat: finish files

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const usersRoutes = require("./routes/usersRoutes");
+app.use("/api", usersRoutes);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
